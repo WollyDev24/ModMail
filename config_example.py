8f1e7f8023a4cea78f87bec3a0d7bbe504500121
WollyDev24 <225042034+WollyDev24@users.noreply.github.com.>
2025-10-26 02:26:38 +0200

    update config file

--- config_example.py
+++ config_example.py
@@ -1,2 +1,4 @@
 # Rename this file to config.py
-token= "123456789010.ABCDEFGHIJK.KLMNOPQ.RSTUVWXYZ" # Replace this with your own token
+TOKEN= "123456789010.ABCDEFGHIJK.KLMNOPQ.RSTUVWXYZ" # Replace this with your own token
+GUILD= 1123456789 # Replace this with your own guild ID
+CATEGORY = 123456789 # Replace this with your own category ID
